4074b82aa9541f2cf15216036d455f82bb314e44
Simeon <yevlakhov@gmail.com>
2022-09-22 05:38:25 +0400

    Initial commit

---
+++ test/reading_test.js
@@ -0,0 +1,48 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Reading users out of the database', (done) => {
+    let joe, maria, alex, zach;
+
+    beforeEach((done) => {
+        alex = new User({ name: 'Alex' });
+        joe = new User({ name: 'Joe' });
+        maria = new User({ name: 'Maria' });
+        zach = new User({ name: 'Zach' });
+
+        Promise.all([alex.save(), joe.save(), maria.save(), zach.save()])
+            .then(() => done());
+    });
+
+    it('finds all users with a name of joe', (done) => {
+        User.find({ name: 'Joe' })
+            .then((users) => {
+                console.log(users); 
+                assert(users[0]._id.toString() === joe._id.toString());
+                done();
+            });
+    });
+
+
+    it('find a user with a particular id', (done) => {
+        User.findOne({ _id: joe._id})
+            .then((user) => {
+                assert(user.name === 'Joe');
+                done();
+            });
+    });
+
+    it('can skip and limit the result set', (done) => {
+        User.find({})
+            .sort({ name: 1 }) //1 for ascending
+            .skip(1)
+            .limit(2)
+            .then((users) => {
+                assert(users.length === 2);
+                assert(users[0].name === 'Joe');
+                assert(users[1].name === 'Maria');
+                done();
+            });
+    });
+});
+
